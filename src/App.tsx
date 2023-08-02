3e6ff0d98b4ee94e7d1f8a67ff8d808f10e41f1b
Yunusov <abubakar.yunusov.47@mail.ru>
2023-08-02 16:20:29 +0300

    Add Right Sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,11 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Routers from "./Routes/Routes";
-import RightSidebar from "./components/rightSidebar/rightSidebar";
 
 function App() {
   return (
     <>
       <Header />
-      <RightSidebar/>
       <Routers />
       <Footer />
     </>
