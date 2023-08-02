e86b29ee954c3f0046cb7f6609569a52745d6ff8
Yunusov <abubakar.yunusov.47@mail.ru>
2023-08-02 16:53:38 +0300

    update right sidebar <Heaght>

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import RightSidebar from "./components/rightSidebar/rightSidebar";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Routers from "./Routes/Routes";
 
 function App() {
   return (
     <>
-      <Header />
+    <RightSidebar/>
+      {/* <Header />
       <div className="homePage">
         <Sidebar />
         <Routers />
-      </div>
+      </div> */}
     </>
   );
 }
