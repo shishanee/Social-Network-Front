864b19513a53f1a8443a9f590796b94d525b1352
shishanee <mansur72095@gmail.com>
2023-08-02 16:24:07 +0300

    code review

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import BtnScrollUp from "./components/BtnScrollUp/BtnScrollUp";
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Routers from "./Routes/Routes";
@@ -6,6 +7,7 @@ import Routers from "./Routes/Routes";
 function App() {
   return (
     <>
+      <BtnScrollUp />
       <Header />
       <div className="homePage">
         <Sidebar />
