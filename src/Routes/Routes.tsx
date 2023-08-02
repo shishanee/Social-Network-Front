daad77c2721801f937a74e3398ca387dbeb4e459
Abdurrahman <timur.navrazov@mail.ru>
2023-08-02 21:01:05 +0300

    сделал верстку хидера и одного блока

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -1,14 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import SignUp from "../components/Sign/SignUp";
 import SignIn from "../components/Sign/SignIn";
-import MainFeed from "../components/MyFeed/MainFeed";
+import MyFeed from "../components/MyFeed/MyFeed";
 
 const Routers = () => {
   return (
     <Routes>
       <Route path="/register" element={<SignUp />} />
       <Route path="/login" element={<SignIn />} />
-      <Route path="/myFeed" element={<MainFeed />} />
+      <Route path="/myFeed" element={<MyFeed />} />
     </Routes>
   );
 };
--- src/components/MyFeed/MainFeed.tsx
+++ src/components/MyFeed/MainFeed.tsx
@@ -1,56 +1,16 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import styles from "./MyFeed.module.scss"
+import styles from "./MyFeed.module.scss";
+import LeftSide from "./LeftSide";
+import RightSide from "./RightSide";
 
 const MainFeed: React.FC = () => {
   return (
-    <div className={styles.app}>
-
-    <div className={styles.allPage}>
-
-      <div className={styles.sideBar}></div>
-
-      <div className={styles.mainContent}>
-
-          <div className={styles.contentHeader}>
-
-              <div className={styles.avaName}>
-
-                  <img className={styles.avaImg} src="https://i.ibb.co/qJBKH3D/Abdurrahman.jpg" alt="ava" />
-                  
-                  <div className={styles.blockWithoutImg}>
-                  <div className={styles.fullName}>
-                      <div className={styles.name}>Abdurrahman Navrazov</div>
-                      <Link to={"#"} className={styles.linkToChangeProfile}>Укажите информацию о себе 
-                     <span className={styles.arrowLink}>  › </span>
-                      </Link>
-                  </div>
-
-                <div className={styles.blockForChanging}>
-              <button className={styles.changeProfile}>Редактировать профиль</button>
-              <button className={styles.yet}> Ещё </button>
-              </div>
-              </div>
-              </div>
-              
-          </div>
-
-          <div className={styles.contentMain}>
-              <div className={styles.leftSide}>
-                <div className={styles.blockPhoto}>
-                    <div className={styles.phptoAndMusic}>
-                        <Link to={"#"} className={styles.photoBut}></Link>
-                        <Link to={"#"} className={styles.musicText}></Link>
-                    </div>
-                </div>
-              </div>
-              <div className={styles.rightSide}></div>
-          </div>
-
-      </div>
+    <div className={styles.contentMain}>
+      <LeftSide />
+      <RightSide />
     </div>
-  </div>
   );
 };
+
 
 export default MainFeed;
