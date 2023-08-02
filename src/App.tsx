ac8fb959721d5c44c4481af505c007ceb60bc2b7
shishanee <mansur72095@gmail.com>
2023-08-02 16:28:04 +0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import BtnScrollUp from "./components/BtnScrollUp/BtnScrollUp";
 import Header from "./components/Header/Header";
+import RightSidebar from "./components/rightSidebar/rightSidebar";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Routers from "./Routes/Routes";
 
@@ -12,6 +13,7 @@ function App() {
       <div className="homePage">
         <Sidebar />
         <Routers />
+        <RightSidebar />
       </div>
     </>
   );
