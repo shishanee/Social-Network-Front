58578b8f09931902fbf77992953728cfcbbab238
shishanee <mansur72095@gmail.com>
2023-08-02 12:09:22 +0300

    code review

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import "./App.css";
 import Header from "./components/Header/Header";
-import Home from "./components/Home/Home";
-import Footer from "./components/Home/Home";
 import Routers from "./Routes/Routes";
 
 function App() {
