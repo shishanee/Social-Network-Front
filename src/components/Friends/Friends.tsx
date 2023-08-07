e5faa9cc165bff28b6c8030fc5a2b22b3cfd282a
shishanee <mansur72095@gmail.com>
2023-08-07 10:58:29 +0300

    code review

--- src/components/Friends/Friends.tsx
+++ src/components/Friends/Friends.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import { useState, useEffect } from "react";
 import styles from "./Friends.module.scss";
 import FriendsCart from "./FriendsCart";
-import test from "../../a/01_18_-_.jpg";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../../app/store";
 import { getUser } from "../../features/userSlice";
+import noimage from "../../../public/noimage.png";
 
 const Friends: React.FC = (): JSX.Element => {
   const dispatch = useDispatch<AppDispatch>();
@@ -15,10 +15,8 @@ const Friends: React.FC = (): JSX.Element => {
   const [actionSubscriptions, setActionSubscriptions] =
     useState<boolean>(false);
 
-  const user = useSelector((item: RootState) => item.user.user);
   const followers = useSelector((item: RootState) => item.user.followers);
   const friends = useSelector((item: RootState) => item.user.friends);
-  console.log(friends);
 
   useEffect(() => {
     dispatch(getUser());
@@ -72,7 +70,11 @@ const Friends: React.FC = (): JSX.Element => {
                 {followers.map((item) => {
                   return (
                     <FriendsCart
-                      image={`http://localhost:4000/${item.image}`}
+                      image={
+                        !item.image
+                          ? noimage
+                          : `http://localhost:4000/${item.image}`
+                      }
                       firstName={item.firstName}
                       lastName={item.lastName}
                       buttonText="Отписать"
