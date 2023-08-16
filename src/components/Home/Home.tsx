7cc509deca143a178f1a8d2267d2842b748d6067
shishanee <mansur72095@gmail.com>
2023-08-16 10:25:30 +0300

    code review

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,27 +1,12 @@
 import React from "react";
 import styles from "./Home.module.scss";
-import { useSelector } from "react-redux";
-import { RootState } from "../../app/store";
-import noimage from "../../../public/noimage.png";
 import PostBlock from "./PostBlock";
 
 const Home: React.FC = (): JSX.Element => {
-  const user = useSelector((state: RootState) => state.user.user);
-  const token = useSelector((state: RootState) => state.application.token);
-
   return (
     <>
       <div className={styles.homePage}>
         <title>Новости</title>
-        <div className={styles.post}>
-          {token && (
-            <img
-              src={user.image ? `http://localhost:4000/${user.image}` : noimage}
-              alt=""
-            />
-          )}
-          <input type="text" placeholder="Что у вас нового?" />
-        </div>
         <PostBlock />
       </div>
     </>
